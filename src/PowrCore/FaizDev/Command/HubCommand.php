f5020b419857f828d153888f70d425ce9db7299c
FaizxDev <75949240+Faizx511x@users.noreply.github.com>
2022-05-28 10:07:59 +0300

    DONE!

---
+++ src/PowrCore/FaizDev/Command/HubCommand.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PowrCore\FaizDev\Command;
+
+use pocketmine\Server;
+use pocketmine\player\Player;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+use pocketmine\plugin\PluginOwned;
+
+use PowrCore\FaizDev\PowrCore;
+
+class HubCommand extends Command implements PluginOwned {
+
+    private $plugin;
+
+    public function __construct(PowrCore $plugin){
+        $this->plugin = $plugin; 
+
+        parent::__construct("hub", 'Teleport you to the server spawn!', null, ["spawn", "lobby"]);
+        $this->setAliases(["spawn", "lobby"]);
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args): void {
+        if ($sender instanceof Player) {
+            $sender->teleport(Server::getInstance()->getWorldManager()->getDefaultWorld()->getSafeSpawn());
+            } else {
+                $sender->sendMessage("Use this command in-game");
+            }
+        }
+
+    public function getOwningPlugin(): PowrCore{
+        return $this->plugin;
+    }
+}
